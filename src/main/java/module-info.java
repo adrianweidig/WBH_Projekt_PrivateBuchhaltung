a05bf5f9f8468e8d8fc87f04102ee4881bcff728
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2025-01-02 03:48:09 +0100

    Projektinitialisierung
    Initialisierung folgender Grundlageneinheiten:
    - IntelliJ Projektumgebung
    - JavaFX Projektrahmen
    - JPro Projektrahmen
    
    via Gradle

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module wbh.wbh_projekt_privatebuchhaltung {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens wbh.wbh_projekt_privatebuchhaltung to javafx.fxml;
+    exports wbh.wbh_projekt_privatebuchhaltung;
+}
